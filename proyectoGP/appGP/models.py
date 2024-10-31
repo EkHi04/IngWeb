79231c6a3687aaf6c56cbe430feb1b38d9d2ada7
EkHi04 <160221030+EkHi04@users.noreply.github.com>
2024-10-31 09:46:15 +0100

    Hurrengo urratsa

--- proyectoGP/appGP/models.py
+++ proyectoGP/appGP/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Tarea(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateField(auto_now_add=True)
+    fecha_entrega = models.DateField(null=True, blank=True)
+    proyecto = models.ForeignKey(Proyecto, related_name='tareas', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, related_name='tareas_usuario', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Usuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telefono = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return self.user.username
